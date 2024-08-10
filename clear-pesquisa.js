928507a033633dda7b20e02e595cbe53a0abc8b4
glaudiaalmeida <glaudiaalmeida@gmail.com>
2024-08-10 20:42:21 -0300

    acertando commits

--- clear-pesquisa.js
+++ clear-pesquisa.js
@@ -1,20 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('pesquisar');
-    const clearButton = document.getElementById('clear__input');
-  
-    input.addEventListener('focus', () => {
-      clearButton.style.display = 'block';
-    });
-  
-    input.addEventListener('blur', () => {
-      if (input.value === '') {
-        clearButton.style.display = 'none';
-      }
-    });
-  
-    clearButton.addEventListener('click', () => {
-      input.value = '';
-      clearButton.style.display = 'none';
-      input.focus();
-    });
+  const input = document.getElementById('pesquisar');
+
+  input.addEventListener('focus', () => {
+    input.placeholder = '';
   });
+
+  input.addEventListener('blur', () => {
+    input.placeholder = 'Qual a sua busca para hoje?';
+  });
+});
